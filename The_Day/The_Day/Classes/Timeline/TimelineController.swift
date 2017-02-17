5c0f0aa62bf321b5e7f612817b1062d688c03ff0
huangmubin <huangmubin@qq.com>
2017-02-17 18:06:32 +0800

    update

---
+++ The_Day/The_Day/Classes/Timeline/TimelineController.swift
@@ -0,0 +1,35 @@
+//
+//  TimelineController.swift
+//  The_Day
+//
+//  Created by 黄穆斌 on 2017/2/17.
+//  Copyright © 2017年 黄穆斌. All rights reserved.
+//
+
+import UIKit
+
+class TimelineController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
